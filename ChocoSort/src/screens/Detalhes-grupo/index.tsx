844eb15e4f5c85fc438241dd562d799dd4e50d67
João Pedro <51719213+JoaoPedro6017@users.noreply.github.com>
2024-06-28 11:53:30 -0300

    Arquivos de projeto - H1
    Arquivos de projeto - H1 - 28/06

---
+++ ChocoSort/src/screens/Detalhes-grupo/index.tsx
@@ -0,0 +1,23 @@
+import { Container, Title } from './styles'
+
+
+
+interface IGroups {
+    id: string,
+    Descricao: string,
+    status: number,
+    imagem: string,
+    nome: string,
+    valor: string,
+    dataRevelacao: string,
+}
+
+
+export function DetailsGroup() {   
+    return (
+        <Container>
+          <Title>Ahhhhhhhhhhhhh</Title>
+        </Container>
+
+    )
+}
